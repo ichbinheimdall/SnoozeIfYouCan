13b2e1fcca45951b0ff7bea7968fef60761bb8ff
ichbinheimdall <ichbinheimdall@users.noreply.github.com>
2026-01-14 19:15:27 +0300

    Initial Commit

---
+++ SnoozeIfYouCan/SnoozeIfYouCanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnoozeIfYouCanApp.swift
+//  SnoozeIfYouCan
+//
+//  Created by HMD Developments on 14.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct SnoozeIfYouCanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
